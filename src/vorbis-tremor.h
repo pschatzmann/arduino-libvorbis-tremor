23e0883566662bacd0f34620c5fc4be0f26c6bc8
pschatzmann <phil.schatzmann@gmail.com>
2024-11-28 19:31:29 +0100

    Remove dependency to AudioTools

--- src/vorbis-tremor.h
+++ src/vorbis-tremor.h
@@ -1,4 +1,6 @@
 #pragma once
-#include "AudioCodecs/ContainerOgg.h"
+//#include "AudioCodecs/ContainerOgg.h"
+#include "opus.h"
+#include "ogg/ogg.h"
 #include "tremor/ivorbisfile.h"
 #include "tremor/ivorbiscodec.h"
